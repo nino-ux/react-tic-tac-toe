bfc49e7a9c1ce49ea7e76d46f749ced30979acd0
nino-ux <nikita.novik.ux@gmail.com>
2026-05-15 17:20:27 -0600

    First steps

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Square from "./Components/Square";
+
+function App() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  return (
+    <>
+      <div className="square-row">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="square-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="square-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Square.jsx
@@ -0,0 +1,14 @@
+function handleButton() {
+// TODO
+}
+
+function Square({value}) {
+    return (
+        <>
+            <button className="square-btn">{value}</button>
+        </>
+    )
+    
+}
+
+export default Square
